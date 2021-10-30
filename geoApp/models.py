0072ee8975887481eed2899806349f8b01aaa4a0
isaacmaredi <isaacmaredi@icloud.com>
2021-10-30 21:59:25 +0200

    addded change pasword functionality

--- geoApp/models.py
+++ geoApp/models.py
@@ -3,7 +3,7 @@ from decimal import Decimal
 from pmlprofile.models import Profile
 
 class Location(models.Model):
-    member = models.ForeignKey(Profile, related_name='location', on_delete=models.CASCADE)
+    member = models.OneToOneField(Profile, on_delete=models.CASCADE)
     lat = models.DecimalField(default=Decimal('0.0'), decimal_places=6, max_digits=8)
     lng = models.DecimalField(default=Decimal('0.0'), decimal_places=6, max_digits=8)
     
--- geoApp/views.py
+++ geoApp/views.py
@@ -4,84 +4,61 @@ from datetime import datetime, time
 from .models import Location
 
 import pandas as pd
-from geopy.geocoders import ArcGIS, Nominatim
+import numpy as np
+
+import json
+
+# def profile_geo(request):
+#     locations = [
+#         [loc.lat, loc.lng, i]
+#         for i, loc in enumerate(Location.objects.all())
+#     ]
+#     mytext = 'This the json enabled map'
+#     context = {
+#         'mytext': mytext,
+#         'locations': json.dumps(locations)
+#         }
+#     print(context)
+#     return render(request, 'geoApp/members.html', context)
 
 def profile_geo(request):
-    qs = Profile.objects.all().values()
-    print(qs)
-    print('#'*10)
+    qs = Location.objects.all()
+    
+    members = []
+    lat = []
+    lng = []
+    address = []
+    for obj in qs:
+        members.append(str(obj))
+        address.append(obj.member.address)
+        lat.append(float(obj.lat))
+        lng.append(float(obj.lng))
+        
+    ltlng = [(lat[i],lng[i]) for i in range(len(lat))]
+
+    loc_dict = Location.objects.values()
+    print(loc_dict)
+
+    names = [name for name in members ]
     
 
-    data = pd.DataFrame(qs)
-    pml_geo = data.filter(['shortname','address','town_city','district_metro'], axis=1)
-    pml_geo.set_index('shortname', inplace=True)
-    # print(pml_geo)
     
-    # print(pml_geo)
-    # name_list = list(data['shortname'])
-    # pml_geo['full_address'] = pml_geo['address']  + ', ' + pml_geo['district_metro']
-
-    # ads = list(pml_geo['full_address'])
-
-    # pml = []
+    objects = qs.values('member','lat','lng')
+    geo_df = pd.DataFrame.from_records(objects)
+    # print(geo_df)
     
     
     
-    # locs = ArcGIS()
     
-    # lats = []
-    # lons = []
+    # print(lats)
+    # print(lons)
     
-    # for lat in ads:
-    #     lat = locs.geocode(lat).latitude
-    #     print(lat)
-    #     lats.append(lat)
-    # for lon in ads:
-    #     print(lon)
-    #     lon = locs.geocode(lon).longitude
-    #     lons.append(lon)
-    # # ends = starts - datetime.now() 
-    
-    # pml_data = list(zip(name_list,ads,lats,lons))
-    # pml_df = pd.DataFrame(pml_data, columns=['member','address','latitude','longitude'])
-    # # pml_df.to_sql(Location) 
-    # pml_geo_csv = pml_df.to_csv('pmlgeo.csv') 
-    # # pml_df.to_sql(Location,)
-    # pml_df.set_index('Member', inplace=True)
-    # print(pml_df)
-    # # print('$'*30)
-    # # print(pml_df)
-    # # print(pml_data)
-    # # print('$'*30)
-    
-    
-    # import folium 
-    
-    # names = list(pml_df['Member'])
-    # address = list(pml_df['Address'])
-    # lat = list(pml_df['Latitude'])
-    # lon = list(pml_df['Longitude'])
-    
-    # pml_map = folium.Map(location = [-25.696769999999958, 28.741820000000075], zoom_start = 9)
-    # fg = folium.FeatureGroup(name = "PML Members Locations")
-    
-    # for lt, ln, addr, name in zip(lat,lon,address,name_list):
-    #     print(lt,ln)
-    #     fg.add_child(folium.CircleMarker(location = [lt,ln], 
-    #                                     popup = (name, addr), radius = 6, color ='blue',
-    #                                     fill_color = 'red', fill_opacity =0.3))
-    # pml_map.add_child(fg)
-    # fg.save('pmlgeo.html')
-    # ends = datetime.now()
-
-    # duration = starts - ends
-    
-
-    # pml_map = pml_map._repr_html_()
-    # ctx = "Testing  csv"
     ctx = "Testing"
     context ={
-        'ctx': ctx,
+        'qs':qs,
+        'members': members,
+        'lat': lat,
+        'lng': lng,
     }
     
     return render(request, 'geoApp/members.html',context)
