2c295891384d4b6c03c4bb1f246c663fd727cb40
isaacmaredi <isaacmaredi@icloud.com>
2021-11-17 17:57:55 +0200

    modified accounts table

--- docs/models.py
+++ docs/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from events.models import Meeting
 
-# Create your models here.
 class Minutes(models.Model):
     title = models.OneToOneField(Meeting, on_delete=models.DO_NOTHING, related_name='meeting')
     date = models.DateField(auto_now_add=True)
@@ -9,4 +8,16 @@ class Minutes(models.Model):
     
     def __str__(self):
         return f'Minutes - {self.title.meeting}'
+
+class Policy(models.Model):
+    title = models.CharField(max_length=250)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    doc_file = models.FileField(upload_to='policies/%Y/%F')
+    
+class Report(models.Model):
+    title = models.CharField(max_length=250)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    doc = models.FileField(upload_to='reports/%Y/%F')
+
+
     
